97985fba6698865233af4d6c7523941d0902eea7
DalerZakirov <kirill283.81.41@gmail.com>
2023-12-10 18:38:09 +0100

    profile page code is added

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,14 +1,65 @@
 import { useEffect } from 'react'
+import { Card, CardContent, Container, Button, Typography, IconButton, InputAdornment, OutlinedInput, FormControl, InputLabel, Snackbar, Alert } from '@mui/material'
 import api from '@/services/api'
 
 const Profile = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [photo, setPhoto] = useState('')
+  const [firstName, setFistName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        // Assuming api.getProfile() returns user details
+        const userProfile = await api.getProfile();
+
+        // Update state with user details
+        setPhoto(userProfile.photo || '');
+        setFirstName(userProfile.first_name || '');
+        setLastName(userProfile.last_name || '');
+        setUsername(userProfile.username || '');
+        setEmail(userProfile.email || '');
+        setPassword(''); // You might want to handle this based on your use case
+      } catch (error) {
+        console.error('Error fetching user profile:', error);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+
   useEffect(() => {
     api.getProfile()
   }, [])
 
-  return <div>
-    <h1>PROFILE</h1>
-  </div>
+ 
+  rreturn (
+    <Container maxWidth="md">
+      <Card>
+        <CardContent>
+          <Typography variant="h4">User Profile</Typography>
+          <div>
+            <img src={photo} alt="User" />
+          </div>
+          <div>
+            <Typography>First Name: {firstName}</Typography>
+            <Typography>Last Name: {lastName}</Typography>
+            <Typography>Username: {username}</Typography>
+            <Typography>Email: {email}</Typography>
+            {/* Add other details as needed */}
+          </div>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 }
+
 
 export default Profile
